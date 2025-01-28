f5eeb34d5d744743df2e222b7b6a7e0deb89ea5e
BuildTools <unconfigured@null.spigotmc.org>
2025-01-29 07:58:54 +0900

    h

--- main.py
+++ main.py
@@ -117,6 +117,7 @@ async def join_command(interaction: discord.Interaction):
             await interaction.response.send_message(f"{channel.name} に移動しました。")
         else:
             global server_statuses
+            await voice_client.connect()
             await interaction.response.send_message(f"{channel.name} に接続しました。")
             server_statuses[interaction.guild.id] = ServerStatus(interaction.guild.id)
         
